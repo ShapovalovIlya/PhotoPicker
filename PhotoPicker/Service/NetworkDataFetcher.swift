391b9c8fb41e4b5f700e717e5210ec3f136bd473
Ilia Shapovalov <greateman@mail.ru>
2022-09-05 15:30:25 +0300

    Add network service

---
+++ PhotoPicker/Service/NetworkDataFetcher.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkDataFetcher.swift
+//  PhotoPicker
+//
+//  Created by Илья Шаповалов on 05.09.2022.
+//
+
+import Foundation
+
+protocol DataFetcherServiceProtocol {
+    
+}
+
+final class DataFetcherService {
+    
+    private let dataFetcher: DataFetcher!
+    private let baseURL = "https://api.unsplash.com/"
+    private let API = APIKeys()
+    
+    init(dataFetcher: DataFetcher = NetworkDataManager()) {
+        self.dataFetcher = dataFetcher
+    }
+    
+    
+}
