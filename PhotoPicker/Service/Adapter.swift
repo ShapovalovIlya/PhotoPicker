8cbef5aa920cce760f20c2fec9d0972d67c71435
Ilia Shapovalov <greateman@mail.ru>
2022-09-09 22:37:13 +0300

    Add .photoConverter() method to adapter class. Refactor .getPhoto() method

--- PhotoPicker/Service/Adapter.swift
+++ PhotoPicker/Service/Adapter.swift
@@ -25,33 +25,47 @@ final class Adapter: AdapterProtocol {
             case .failure(let error):
                 complition(.failure(error))
             case .success(let decodedPhotos):
-                var photosArray = [PhotoModel]()
-                guard let photos = decodedPhotos else { return }
-                
-                for photo in photos {
-                    
-                    guard
-                        let thumbImageURL = URL(string: photo.imageURLs.thumb),
-                        let regularImageURL = URL(string: photo.imageURLs.regular)
-                    else {
-                        continue
-                    }
-                    
-                    let newPhoto = PhotoModel(
-                        id: photo.id,
-                        createAt: photo.createdAt,
-                        backgroundHEX: photo.backgroundColor,
-                        downloads: photo.downloads ?? 0,
-                        location: photo.location?.name ?? "unknown",
-                        thumbImageURL: thumbImageURL,
-                        regularImageURL: regularImageURL,
-                        author: photo.author.name
-                    )
-                    photosArray.append(newPhoto)
-                }
+                guard let photosArray = self.photoDataConverter(decodedPhotos: decodedPhotos) else { return }
                 complition(.success(photosArray))
             }
+            
         }
     }
     
+    
+    func getDetailPhoto(withId id: String, complition: @escaping(Result<PhotoModel,Error>) -> Void) {
+        
+    }
+    
+}
+
+private extension Adapter {
+    
+    func photoDataConverter(decodedPhotos: [PhotoData]?) -> [PhotoModel]? {
+        var photosArray = [PhotoModel]()
+        guard let photos = decodedPhotos else { return nil }
+        
+        for photo in photos {
+            
+            guard
+                let thumbImageURL = URL(string: photo.imageURLs.thumb),
+                let regularImageURL = URL(string: photo.imageURLs.regular)
+            else {
+                continue
+            }
+            
+            let newPhoto = PhotoModel(
+                id: photo.id,
+                createAt: photo.createdAt,
+                backgroundHEX: photo.backgroundColor,
+                downloads: photo.downloads ?? 0,
+                location: photo.location?.name ?? "unknown",
+                thumbImageURL: thumbImageURL,
+                regularImageURL: regularImageURL,
+                author: photo.author.name
+            )
+            photosArray.append(newPhoto)
+        }
+        return photosArray
+    }
 }
